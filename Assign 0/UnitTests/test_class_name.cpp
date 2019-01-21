d5e1911b76788de24810a61880f679fdb00982ae
SciNutz <46631975+SciNutz@users.noreply.github.com>
2019-01-21 07:04:43 -0800

    Add files via upload

---
+++ Assign 0/UnitTests/test_class_name.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "crt_check_memory.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit_tests
+{		
+	TEST_CLASS(test_class_name)
+	{
+	public:
+		
+		TEST_METHOD(test_method_name)
+		{
+			// Arrange
+			crt_check_memory check;
+
+
+			// Act
+
+
+			// Assert
+
+		}
+
+	};
+}
